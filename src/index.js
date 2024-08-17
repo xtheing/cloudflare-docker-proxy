ce20bb2b0ee00f8a9157218af4edbb286118dcec
theing <934320370@qq.com>
2024-08-18 00:26:29 +0800

    change routes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.theing.cc": dockerHub,
+  "quay.theing.cc": "https://quay.io",
+  "gcr.theing.cc": "https://gcr.io",
+  "k8s-gcr.theing.cc": "https://k8s.gcr.io",
+  "k8s.theing.cc": "https://registry.k8s.io",
+  "ghcr.theing.cc": "https://ghcr.io",
+  "cloudsmith.theing.cc": "https://docker.cloudsmith.io",
+  "ecr.theing.cc": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.theing.cc": dockerHub,
 };
 
 function routeByHosts(host) {
